85f9e5b42728d43fe0ea690867145b735c5ee161
shion200 <usushio2002@gmail.com>
2023-08-10 13:03:43 +0900

    Send score to DB

---
+++ Assets/lanking.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using NCMB;
+
+public class lanking : MonoBehaviour
+{
+
+    public string currentUserName;
+
+    [SerializeField]
+    int score = 100;
+    NCMBObject data = new NCMBObject("data");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Return)){
+            Debug.Log("スペースキーが押されました。");
+
+        // NCMBUser currentUser = NCMBUser.CurrentUser;
+
+        // if(currentUser != null) {
+        //     UnityEngine.Debug.Log("ログイン中のユーザー：" + currentUser.UserName);
+        // } else {
+        //     UnityEngine.Debug.Log ("未ログインまたは取得に失敗");
+        // }
+
+            data["score"] = score;
+            data["UserName"] = currentUserName;
+            data.SaveAsync();
+
+            return;
+        }
+    }
+}
