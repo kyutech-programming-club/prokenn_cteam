d785b8d05344a5802d6f5afa3ac2e9d295f5d7ce
NakaAwjr <n.sadanana@gmail.com>
2023-08-10 15:15:14 +0900

    first commit

---
+++ Assets/Develop/PracticeScene/Script/MeasureText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeasureText : MonoBehaviour
+{
+    private GameObject _player;
+    private Text _text;
+    private int _distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.Find("Player");
+        _text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = (int)-_player.transform.position.y;
+        _text.text = _distance.ToString() + "m";
+    }
+}
---
+++ Assets/Develop/PracticeScene/Script/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    private Dropdown _stageSelect;
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+    {
+        _stageSelect = GameObject.Find("StageDropdown").GetComponent<Dropdown>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+ 
+    void Update()
+    {
+        //重力加速度の変更
+        if(_stageSelect.value == 0)
+        {
+            _rigidbody.gravityScale = 9.8f;
+        }
+        else if(_stageSelect.value == 1)
+        {
+            _rigidbody.gravityScale = 1.62f;
+        }
+
+    }
+
+    public void PushStart()
+    {
+        _rigidbody.isKinematic = false;
+    }
+    
+    public void PushStop()
+    {
+        _rigidbody.velocity = Vector2.zero;
+        _rigidbody.angularVelocity = 0;
+        _rigidbody.isKinematic = true;
+    }
+
+    public void PushReset()
+    {
+        _rigidbody.velocity = Vector2.zero;
+        _rigidbody.angularVelocity = 0;
+        transform.position = new Vector3(0, 0, 100);
+        _rigidbody.isKinematic = false;
+    }
+}
