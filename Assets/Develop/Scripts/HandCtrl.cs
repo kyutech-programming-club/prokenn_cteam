07bea681b9f21cc5079b8a910419f37d629fc1c6
IwamotoKakeru <kganpipi@gmail.com>
2023-08-11 18:39:59 +0900

    Add Hand

---
+++ Assets/Develop/Scripts/HandCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandCtrl : MonoBehaviour
+{
+
+    private HandSprite handSprite;
+    private HingeJoint2D hingeJoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handSprite = this.gameObject.GetComponent<HandSprite>();
+        hingeJoint = this.gameObject.GetComponent<HingeJoint2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            handSprite.OpenHand();
+            hingeJoint.enabled = false;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            handSprite.CloseHand();
+        }
+    }
+}
---
+++ Assets/Develop/Scripts/HandSprite.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandSprite : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private GameObject TargetObject;
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField] private Sprite OpenSprite;
+    [SerializeField] private Sprite CloseSprite;
+    void Start()
+    {
+        TargetObject = this.gameObject;
+        spriteRenderer = TargetObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void CheckExistance(){
+        if (TargetObject == null)
+        {
+            return;
+        }
+        if (OpenSprite == null || CloseSprite == null)
+        {
+            return;
+        }
+        if (spriteRenderer == null)
+        {
+            return;
+        }
+    }
+
+    public void OpenHand()
+    {
+        CheckExistance();
+        spriteRenderer.sprite = OpenSprite;
+    }
+
+    public void CloseHand()
+    {
+        CheckExistance();
+        spriteRenderer.sprite = CloseSprite;
+    }
+}
