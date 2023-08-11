7290f906851b97f42503a8ecfd5ca4e91802a380
IwamotoKakeru <kganpipi@gmail.com>
2023-08-11 15:01:40 +0900

    Add Caracter

---
+++ Assets/Develop/Scripts/CharacterTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTest : MonoBehaviour
+{
+    Vector3 prev;
+    void Update()
+    {
+        Vector3 p = Camera.main.ScreenPointToRay(Input.mousePosition).origin;
+        p.z = 0;
+
+        if (Input.GetMouseButton(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = (p - this.prev) * 80;
+        }
+        this.prev = p;
+    }
+}
