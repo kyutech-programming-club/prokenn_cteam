7bf2894d8675f86706ed02d6cda6c4524ef36638
NakaAwjr <n.sadanana@gmail.com>
2023-08-12 14:19:57 +0900

    スコアの保存とシーン移動の設定

--- Assets/Develop/Scripts/Controller.cs
+++ Assets/Develop/Scripts/Controller.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
     private GameObject _player;
     private GameDirector _gameDirector;
+    private SaveResult _saveResult;
     [SerializeField] private Result _result;
     [SerializeField] private bool _isStop;
+    [SerializeField] private bool _isStart;
     // Start is called before the first frame update
     void Start()
     {
+        _saveResult = GetComponent<SaveResult>();
         _player = GameObject.FindWithTag("Player");
-        _gameDirector = GameObject.FindWithTag("GameController").GetComponent<GameDirector>();
-        _isStop = true;
+        _gameDirector = GetComponent<GameDirector>();
+        _isStop = false;
+        _isStart = false;
     }
 
     // Update is called once per frame
@@ -22,16 +27,23 @@ public class Controller : MonoBehaviour
         //controller
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Mouse0))
         {
-            if (_isStop)
+            if (!_isStart)
             {
+                _isStart = true;
                 _isStop = false;
                 _player.GetComponent<Player>().PushStart();
             }
-            else
+            else if (!_isStop)
             {
+                _isStop = true;
                 _player.GetComponent<Player>().PushStop();
                 _gameDirector.GameStop();
                 _result.GameStop();
+                _saveResult.Save();
+            }
+            else
+            {
+                SceneManager.LoadScene("lanking");
             }
         }
     }
