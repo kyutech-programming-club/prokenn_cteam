cf069903b7e2a72582fa04ebf8b6cd3ac2318a42
NakaAwjr <n.sadanana@gmail.com>
2023-08-12 11:59:53 +0900

    スタート・ストップの変更、結果用ポップアップ

--- Assets/Develop/Scripts/PopupText.cs
+++ Assets/Develop/Scripts/PopupText.cs
@@ -17,16 +17,17 @@ public class PopupText : MonoBehaviour
         _gameDirector = GameObject.FindWithTag("GameController").GetComponent<GameDirector>();
         _transformy = transform.position.y;
         transform.position = new Vector3(transform.position.x, _transformy + 1000, transform.position.z);
-        DOVirtual.DelayedCall(0.1f, () =>
-        {
-            _text.text = _gameDirector.TagetMeasure.ToString() + "mで止めろ！";
-            transform.DOMoveY(_transformy, _speed);
-        });
 
         DOVirtual.DelayedCall(4.0f, () =>
         {
             transform.DOMoveY(_transformy + 1000, _speed);
         });
+    }
+
+    public void Start_(int taget)
+    {
+        _text.text = taget.ToString() + "mで止めろ！";
+        transform.DOMoveY(_transformy, _speed);
     }
 
     // Update is called once per frame
---
+++ Assets/Develop/Scripts/UIText.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIText : MonoBehaviour
+{
+    private GameObject _player;
+    [SerializeField] private Text _measureText;
+    [SerializeField] private Text _tagetText;
+    private int _distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = (int)-_player.transform.position.y;
+        if (_distance < 300)
+        {
+            _measureText.text = _distance.ToString() + "m";
+        }
+        else
+        {
+            _measureText.text = "?m";
+        }
+        
+    }
+
+    public void SetTagetText(int taget)
+    {
+        _tagetText.text = taget.ToString() + "mで止めろ！";
+    }
+}
