58056a31ba24bb6ef928f03129fd1c32f460e7c3
NakaAwjr <n.sadanana@gmail.com>
2023-08-12 16:39:48 +0900

    タイトルに戻る

---
+++ Assets/Develop/Scripts/BackTitle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackTitle : MonoBehaviour
+{
+    [SerializeField] private string _scene;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            SceneManager.LoadScene(_scene);
+        }
+    }
+}
