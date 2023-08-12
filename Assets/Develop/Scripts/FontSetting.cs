82dc6cce4d5241f4b3990cf74a9a79b803c184d7
NakaAwjr <n.sadanana@gmail.com>
2023-08-12 14:32:46 +0900

    フォント設定

---
+++ Assets/Develop/Scripts/FontSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FontSetting : MonoBehaviour
+{
+    [SerializeField] private Font _font;
+    [SerializeField] private List<GameObject> _textObjects;
+    [SerializeField] private Color _color;
+
+    private void Awake()
+    {
+        foreach (GameObject textObject in _textObjects)
+        {
+            textObject.GetComponent<UnityEngine.UI.Text>().font = _font;
+            textObject.GetComponent<UnityEngine.UI.Text>().color = _color;
+        }
+    }
+}
